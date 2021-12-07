ab3a48f62cd41b812dc1c450a750d6165e4ffac3
Luana Nunes <lununes.s@outlook.com>
2021-12-07 17:07:41 -0300

    Adding Python Calculator

---
+++ Cap03/calculadora.py
@@ -0,0 +1,31 @@
+# Calculadora em Python
+
+print('********************* Python Calculator *********************\n\n') 
+
+
+print('Selecione o número da operação desejada: \n')
+
+print('1 - Soma')
+print('2 - Subtração')
+print('3 - Multiplicação')
+print('4 - Divisão\n')
+
+a = int(input('Digite sua opção (1/2/3/4):  '))
+
+x = int(input('\nDigite o primeiro número:  '))
+
+y = int(input('\nDigite o segundo número:  ')) 
+
+if a == 1:
+    somar = x + y
+    print('\n{} + {} = {}'.format(x, y, somar))
+elif a == 2:
+    subtrair = x - y
+    print('\n{} - {} = {}'.format(x, y, subtrair))
+elif a == 3:
+    multiplicar = x * y
+    print('\n{} * {} = {}'.format(x, y, multiplicar))
+elif a == 4:
+    dividir = x/y
+    print('\n{} / {} = {}'.format(x, y, dividir))
+else: print ('\nA opção escolhida é inválida!')
